d6263cbedfc85ff9dd32d5270a567d4b25dbd2d6
Sebastian Waisbrot <seppo0010@gmail.com>
2016-05-10 19:32:46 -0300

    Add settings

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+var settings = {};
+chrome.storage.local.get('settings', function(data) {
+    if (data.settings) {
+        settings.criteria = data.settings.criteria || '@PantallaBoca';
+        settings.left = data.settings.left || 10;
+        settings.top = data.settings.top || 10;
+        refresh();
+    }
+});
+
 var loadImage = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
@@ -11,7 +21,7 @@ var loadImage = function(url, callback) {
 };
 
 var getPhotos = function(callback) {
-    $.ajax('https://twitter.com/search?f=images&vertical=default&q=pantallaboca&src=typd', {
+    $.ajax('https://twitter.com/search?f=images&vertical=default&q=' + encodeURI(settings.criteria) + '&src=typd', {
         success: function(data) {
             var images = $(data).find('.AdaptiveStreamGridImage').map(function(i, el) {
                 var $el = $(el);
@@ -25,9 +35,14 @@ var getPhotos = function(callback) {
     });
 }
 
-$(function() {
+var refresh = function() {
+    if (!settings.criteria) return;
+    $('#current').css({
+        left: settings.left,
+        top: settings.top
+    });
+    var $photos = $('#photos').empty();
     getPhotos(function(photos) {
-        var $photos = $('#photos');
         $.each(photos, function(i, photo) {
             loadImage(photo.url, function(image) {
                 $photos.append(image);
@@ -45,10 +60,31 @@ $(function() {
             });
         });
     });
+};
+
+$(function() {
     $(document).on('keydown keyup', function(ev) {
         if (ev.keyCode === 27) {
             $('#current').empty();
             ev.preventDefault();
         }
     });
+    $('#showSettings').click(function(ev) {
+        ev.preventDefault();
+        var $settings = $('#settings');
+        if ($settings.is(':visible')) {
+            settings.criteria = $settings.find('#criteria').val();
+            settings.left = parseInt($settings.find('#left').val(), 10);
+            settings.top = parseInt($settings.find('#top').val(), 10);
+            chrome.storage.local.set({settings: settings});
+            refresh();
+            $settings.hide();
+        } else {
+            $settings.find('#criteria').val(settings.criteria);
+            $settings.find('#left').val(settings.left);
+            $settings.find('#top').val(settings.top);
+            $settings.show();
+        }
+    });
+    $('#settings').hide();
 });
